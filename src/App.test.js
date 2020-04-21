79e412e9c1694d61bcc19a8818dae7f0fca24188
Jeffrey Luong <jjck1988@gmail.com>
2020-04-20 22:53:04 -0400

    Start adding tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import RemoteValue from './lib/RemoteValue';
+import { List } from 'immutable';
+import Order from './models/order';
+
+const mockStore = configureStore();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const store = mockStore({
+    orders: new RemoteValue({
+      isFetching: false,
+      didEverLoad: true,
+      didInvalidate: false,
+      error: '',
+      value: List([new Order()])
+    })
+  });
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
 });
