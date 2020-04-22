ac36626ba25aa2bd414e239d46a38e3a98de3730
Jeffrey Luong <jjck1988@gmail.com>
2020-04-21 22:16:51 -0400

    Remove commented code

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import * as React from 'react';
 import './App.scss';
 import ProductSalesByStateTable from './components/ProductSalesByStateTable';
 
-// declare module '*.scss';
-
 const App = (): JSX.Element => (
   <div className="App">
     <ProductSalesByStateTable />
